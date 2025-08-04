c9f1c4dac6a9a9f7f737f6cb3bae6bdefe80c349
Quinten Yearsley <qyearsley@gmail.com>
2025-08-03 17:25:44 -0700

    Make changes in style by using dialog with Cursor IDE

--- js/truthtable.js
+++ js/truthtable.js
@@ -1,4 +1,4 @@
-"use strict";
+"use strict"
 /**
  * truthtable.js
  */
@@ -6,101 +6,100 @@
  * Binary operators, which take two arguments.
  */
 const BINARY_OPERATORS = {
-    and: (x, y) => x && y,
-    eq: (x, y) => x == y,
-    implies: (x, y) => !x || y,
-    nand: (x, y) => !(x && y),
-    or: (x, y) => x || y,
-    xor: (x, y) => x != y,
-};
+  and: (x, y) => x && y,
+  eq: (x, y) => x == y,
+  implies: (x, y) => !x || y,
+  nand: (x, y) => !(x && y),
+  or: (x, y) => x || y,
+  xor: (x, y) => x != y,
+}
 /**
  * Unary operators, which take a single argument.
  */
 const UNARY_OPERATORS = {
-    not: (x) => !x,
-};
+  not: (x) => !x,
+}
 /**
  * Operators and their precedence.
  * Higher precedence means the operator binds more tightly,
  * so it is evaluated first.
  */
 const PRECEDENCE = {
-    and: 1,
-    or: 1,
-    nand: 2,
-    implies: 2,
-    eq: 2,
-    xor: 2,
-    not: 3,
-};
+  and: 1,
+  or: 1,
+  nand: 2,
+  implies: 2,
+  eq: 2,
+  xor: 2,
+  not: 3,
+}
 /**
  * A BoolExpr is an expression that can be evaluated to
  * a boolean value.
  */
-class BoolExpr {
-}
+class BoolExpr {}
 class BoolExprConst extends BoolExpr {
-    constructor(value) {
-        super();
-        this.value = value;
-    }
-    eval() {
-        return this.value;
-    }
-    vars() {
-        return [];
-    }
+  constructor(value) {
+    super()
+    this.value = value
+  }
+  eval() {
+    return this.value
+  }
+  vars() {
+    return []
+  }
 }
 class BoolExprVar extends BoolExpr {
-    constructor(varname) {
-        super();
-        this.varname = varname;
+  constructor(varname) {
+    super()
+    this.varname = varname
+  }
+  eval(map) {
+    if (this.varname in map) {
+      return map[this.varname]
     }
-    eval(map) {
-        if (this.varname in map) {
-            return map[this.varname];
-        }
-        throw new Error("Undefined variable: " + this.varname);
-    }
-    vars() {
-        return [this.varname];
-    }
+    throw new Error("Undefined variable: " + this.varname)
+  }
+  vars() {
+    return [this.varname]
+  }
 }
 class BoolExprUnary extends BoolExpr {
-    constructor(op, arg) {
-        super();
-        this.op = op;
-        this.arg = arg;
-    }
-    eval(map) {
-        const func = UNARY_OPERATORS[this.op];
-        return func(this.arg.eval(map));
-    }
-    vars() {
-        return this.arg.vars();
-    }
+  constructor(op, arg) {
+    super()
+    this.op = op
+    this.arg = arg
+  }
+  eval(map) {
+    const func = UNARY_OPERATORS[this.op]
+    return func(this.arg.eval(map))
+  }
+  vars() {
+    return this.arg.vars()
+  }
 }
 class BoolExprBinary extends BoolExpr {
-    constructor(op, left, right) {
-        super();
-        this.op = op;
-        this.left = left;
-        this.right = right;
+  constructor(op, left, right) {
+    super()
+    this.op = op
+    this.left = left
+    this.right = right
+  }
+  eval(map) {
+    const func = BINARY_OPERATORS[this.op]
+    return func(this.left.eval(map), this.right.eval(map))
+  }
+  vars() {
+    // Get variables from both arguments and de-dupe.
+    const all = this.left.vars()
+    for (let v of this.right.vars()) {
+      if (!all.includes(v)) {
+        all.push(v)
+      }
     }
-    eval(map) {
-        const func = BINARY_OPERATORS[this.op];
-        return func(this.left.eval(map), this.right.eval(map));
-    }
-    vars() {
-        // Get variables from both arguments and de-dupe.
-        const all = this.left.vars();
-        for (let v of this.right.vars()) {
-            if (!all.includes(v)) {
-                all.push(v);
-            }
-        }
-        return all;
-    }
+    return all
+  }
 }
 /**
  * Parse a boolean expression expressed in infix notation.
@@ -112,72 +111,74 @@ class BoolExprBinary extends BoolExpr {
  * @return BoolExpr - The parsed expression.
  */
 function parseInfix(str) {
-    const tokens = tokenize(str);
-    if (tokens.length === 0) {
-        throw new Error("Empty expression");
-    }
-    const postfix = shuntingYard(tokens);
-    return parsePostfix(postfix);
+  const tokens = tokenize(str)
+  if (tokens.length === 0) {
+    throw new Error("Empty expression")
+  }
+  const postfix = shuntingYard(tokens)
+  return parsePostfix(postfix)
 }
 /**
  * Converts an infix expression to postfix, using the shunting yard algorithm.
  */
 function shuntingYard(tokens) {
-    const output = [];
-    const stack = [];
-    for (let token of tokens) {
-        processToken(token, output, stack);
+  const output = []
+  const stack = []
+  for (let token of tokens) {
+    processToken(token, output, stack)
+  }
+  while (stack.length > 0) {
+    const operator = stack.pop()
+    if (operator === "(" || operator === ")") {
+      throw new Error("Mismatched parentheses")
     }
-    while (stack.length > 0) {
-        const operator = stack.pop();
-        if (operator === "(" || operator === ")") {
-            throw new Error("Mismatched parentheses");
-        }
-        if (operator !== undefined) {
-            output.push(operator);
-        }
+    if (operator !== undefined) {
+      output.push(operator)
     }
-    return output;
+  }
+  return output
 }
 /**
  * Process one token in the shunting yard algorithm.
  */
 function processToken(token, output, stack) {
-    if (token in UNARY_OPERATORS) {
-        stack.push(token);
-        return;
+  if (token in UNARY_OPERATORS) {
+    stack.push(token)
+    return
+  }
+  if (token in BINARY_OPERATORS) {
+    while (
+      stack.length > 0 &&
+      stack[stack.length - 1] !== "(" &&
+      PRECEDENCE[token] <= PRECEDENCE[stack[stack.length - 1]]
+    ) {
+      const op = stack.pop()
+      if (typeof op === "string") {
+        output.push(op)
+      }
     }
-    if (token in BINARY_OPERATORS) {
-        while (stack.length > 0 &&
-            stack[stack.length - 1] !== "(" &&
-            PRECEDENCE[token] <= PRECEDENCE[stack[stack.length - 1]]) {
-            const op = stack.pop();
-            if (typeof op === "string") {
-                output.push(op);
-            }
-        }
-        stack.push(token);
-        return;
+    stack.push(token)
+    return
+  }
+  if (token === "(") {
+    stack.push(token)
+    return
+  }
+  if (token === ")") {
+    while (stack.length > 0 && stack[stack.length - 1] !== "(") {
+      const item = stack.pop()
+      if (item !== undefined) {
+        output.push(item)
+      }
     }
-    if (token === "(") {
-        stack.push(token);
-        return;
+    if (stack.length === 0) {
+      throw new Error("Mismatched parentheses")
     }
-    if (token === ")") {
-        while (stack.length > 0 && stack[stack.length - 1] !== "(") {
-            const item = stack.pop();
-            if (item !== undefined) {
-                output.push(item);
-            }
-        }
-        if (stack.length === 0) {
-            throw new Error("Mismatched parentheses");
-        }
-        stack.pop();
-        return;
-    }
-    // If we get here, it's a constant or a variable.
-    output.push(token);
+    stack.pop()
+    return
+  }
+  // If we get here, it's a constant or a variable.
+  output.push(token)
 }
 /**
  * Parse a postfix expression to a BoolExpr.
@@ -186,32 +187,29 @@ function processToken(token, output, stack) {
  * is interpreted as "not (true or false)".
  */
 function parsePostfix(tokens) {
-    const stack = [];
-    for (const token of tokens) {
-        if (token in UNARY_OPERATORS) {
-            const operand = stack.pop();
-            if (operand !== undefined) {
-                stack.push(new BoolExprUnary(token, operand));
-            }
-        }
-        else if (token in BINARY_OPERATORS) {
-            const operand2 = stack.pop();
-            const operand1 = stack.pop();
-            if (operand1 !== undefined && operand2 !== undefined) {
-                stack.push(new BoolExprBinary(token, operand1, operand2));
-            }
-        }
-        else if (token === "true" || token === "false") {
-            stack.push(new BoolExprConst(token === "true"));
-        }
-        else {
-            stack.push(new BoolExprVar(token));
-        }
+  const stack = []
+  for (const token of tokens) {
+    if (token in UNARY_OPERATORS) {
+      const operand = stack.pop()
+      if (operand !== undefined) {
+        stack.push(new BoolExprUnary(token, operand))
+      }
+    } else if (token in BINARY_OPERATORS) {
+      const operand2 = stack.pop()
+      const operand1 = stack.pop()
+      if (operand1 !== undefined && operand2 !== undefined) {
+        stack.push(new BoolExprBinary(token, operand1, operand2))
+      }
+    } else if (token === "true" || token === "false") {
+      stack.push(new BoolExprConst(token === "true"))
+    } else {
+      stack.push(new BoolExprVar(token))
     }
-    if (stack.length !== 1) {
-        throw new Error("Invalid expression");
-    }
-    return stack[0];
+  }
+  if (stack.length !== 1) {
+    throw new Error("Invalid expression")
+  }
+  return stack[0]
 }
 /**
  * Tokenize a string into an array of tokens.
@@ -221,49 +219,48 @@ function parsePostfix(tokens) {
  * by spaces.
  */
 function tokenize(str) {
-    const tokens = [];
-    let token = "";
-    for (let c of str) {
-        if (c === " ") {
-            if (token !== "") {
-                tokens.push(token);
-                token = "";
-            }
-        }
-        else if (c === "(" || c === ")") {
-            if (token !== "") {
-                tokens.push(token);
-                token = "";
-            }
-            tokens.push(c);
-        }
-        else {
-            token += c;
-        }
+  const tokens = []
+  let token = ""
+  for (let c of str) {
+    if (c === " ") {
+      if (token !== "") {
+        tokens.push(token)
+        token = ""
+      }
+    } else if (c === "(" || c === ")") {
+      if (token !== "") {
+        tokens.push(token)
+        token = ""
+      }
+      tokens.push(c)
+    } else {
+      token += c
     }
-    if (token !== "") {
-        tokens.push(token);
-    }
-    return tokens;
+  }
+  if (token !== "") {
+    tokens.push(token)
+  }
+  return tokens
 }
 /**
  * Return an array of all combinations of false and true of length n.
  */
 function listCombinations(n) {
-    if (n == 0) {
-        return [[]];
-    }
-    else {
-        return prependToAll(false, listCombinations(n - 1)).concat(prependToAll(true, listCombinations(n - 1)));
-    }
+  if (n == 0) {
+    return [[]]
+  } else {
+    return prependToAll(false, listCombinations(n - 1)).concat(
+      prependToAll(true, listCombinations(n - 1))
+    )
+  }
 }
 /**
  * Prepend something to each array in a 2d array.
  */
 function prependToAll(x, arrs) {
-    return arrs.map(function (subarr) {
-        return [x].concat(subarr);
-    });
+  return arrs.map(function (subarr) {
+    return [x].concat(subarr)
+  })
 }
 /**
  * TruthTable represents a truth table which can be displayed on a page.
@@ -271,52 +268,52 @@ function prependToAll(x, arrs) {
  * The constructor parses an expression and builds the table.
  */
 class TruthTable {
-    /**
-     * Construct a truth table.
-     *
-     * @param {string} str - An infix expression to parse, like "(not a) or b".
-     * @throws Error if the expression is invalid.
-     */
-    constructor(str) {
-        const expr = parseInfix(str);
-        const vars = expr.vars();
-        const combinations = listCombinations(vars.length);
-        this._rows = [vars.concat([str])];
-        for (let c of combinations) {
-            const map = {};
-            for (let i = 0; i < vars.length; i++) {
-                map[vars[i]] = c[i];
-            }
-            const result = expr.eval(map);
-            const values = c.concat([result]);
-            this.rows.push(values.map((b) => b.toString()));
-        }
+  /**
+   * Construct a truth table.
+   *
+   * @param {string} str - An infix expression to parse, like "(not a) or b".
+   * @throws Error if the expression is invalid.
+   */
+  constructor(str) {
+    const expr = parseInfix(str)
+    const vars = expr.vars()
+    const combinations = listCombinations(vars.length)
+    this._rows = [vars.concat([str])]
+    for (let c of combinations) {
+      const map = {}
+      for (let i = 0; i < vars.length; i++) {
+        map[vars[i]] = c[i]
+      }
+      const result = expr.eval(map)
+      const values = c.concat([result])
+      this.rows.push(values.map((b) => b.toString()))
     }
-    get rows() {
-        return this._rows;
+  }
+  get rows() {
+    return this._rows
+  }
+  toDOMTable() {
+    const table = document.createElement("table")
+    for (let row of this._rows) {
+      var tr = document.createElement("tr")
+      for (let col of row) {
+        var td = document.createElement("td")
+        td.appendChild(document.createTextNode(col))
+        tr.appendChild(td)
+      }
+      table.appendChild(tr)
     }
-    toDOMTable() {
-        const table = document.createElement("table");
-        for (let row of this._rows) {
-            var tr = document.createElement("tr");
-            for (let col of row) {
-                var td = document.createElement("td");
-                td.appendChild(document.createTextNode(col));
-                tr.appendChild(td);
-            }
-            table.appendChild(tr);
-        }
-        return table;
-    }
+    return table
+  }
 }
 module.exports = {
-    BoolExpr: BoolExpr,
-    BoolExprConst: BoolExprConst,
-    BoolExprVar: BoolExprVar,
-    BoolExprUnary: BoolExprUnary,
-    BoolExprBinary: BoolExprBinary,
-    TruthTable: TruthTable,
-    parseInfix: parseInfix,
-    tokenize: tokenize,
-    listCombinations: listCombinations,
-};
+  BoolExpr: BoolExpr,
+  BoolExprConst: BoolExprConst,
+  BoolExprVar: BoolExprVar,
+  BoolExprUnary: BoolExprUnary,
+  BoolExprBinary: BoolExprBinary,
+  TruthTable: TruthTable,
+  parseInfix: parseInfix,
+  tokenize: tokenize,
+  listCombinations: listCombinations,
+}
